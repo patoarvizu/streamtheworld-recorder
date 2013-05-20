2b0386ca594ce3dbe9cc121250dd1d295af5cc96
Pato Arvizu <patoarvizu@gmail.com>
2013-05-20 01:54:42 -0400

    Add timezone info

--- streamtheworld.pp
+++ streamtheworld.pp
@@ -7,3 +7,16 @@ package { "python":
 package { "mutt":
   ensure => installed
 }
+package { "tzdata":
+  ensure => installed
+}
+file { "timezone":
+  path => "/etc/timezone",
+  content => "America/New_York",
+  notify => Exec["set-timezone"]
+}
+exec { "set-timezone":
+  command => "/usr/sbin/dpkg-reconfigure -f noninteractive tzdata",
+  require => File["timezone"],
+  subscribe => File["timezone"]
+}
