b6527feb5acedc63d966927375f7a213ef42cfcc
patoarvizu <patoarvizu@gmail.com>
2013-05-30 12:32:56 -0400

    Add user data parsing to recording script

--- streamtheworld-scheduled.sh
+++ streamtheworld-scheduled.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+source $(dirname $BASH_SOURCE)/parse-user-data.sh
+
 date=`date +%d/%m/%y\ %H:%M`
 /usr/bin/python streamtheworld.py $1 r $2
 echo $date | mutt -s "$1 $date" -a `ls -1t *$1*mp3 | head -1` -- patoarvizu@gmail.com
