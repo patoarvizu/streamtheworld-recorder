73b053558e58b0166f5b864ebf5efec28a9e3fbe
patoarvizu <patoarvizu@gmail.com>
2019-01-05 20:15:24 -0500

    Have to add 'enable_dns_hostnames' to allow nodes to join the cluster

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "stwr" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
   tags = "${
     map(
       "Name", "${local.cluster_name}-node",
