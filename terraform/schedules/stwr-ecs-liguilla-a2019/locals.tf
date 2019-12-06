9f6c9b694e824f3df014717dec5970ddeefde3ef
patoarvizu <patoarvizu@gmail.com>
2019-12-06 16:43:57 -0500

    sf-ida-monterrey-vs-necaxa

--- terraform/schedules/stwr-ecs-liguilla-a2019/locals.tf
+++ terraform/schedules/stwr-ecs-liguilla-a2019/locals.tf
@@ -2,5 +2,6 @@ locals {
     liguilla_a2019_schedule = {
         "cf-ida-monterrey-vs-santos" = "0 0 29 NOV ? 2019"
         "cf-vuelta-tigres-vs-america" = "0 2 2 DEC ? 2019"
+        "sf-ida-monterrey-vs-necaxa" = "0 2 5 DEC ? 2019"
     }
 }
